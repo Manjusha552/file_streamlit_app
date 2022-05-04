cd7e6d37b4061a06844d78d43f75e7319e5a4d3e
GUDIPATI MANJUSHA <104416801+Manjusha552@users.noreply.github.com>
2022-05-04 18:55:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,39 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.title('My MOM new healthy diner')
+streamlit.title('My Mom\'s new healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-#create the repeatable block code (called function)
-def get_fruityvice_data(fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('what fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("please select a fruit to get information")
-  else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()
+
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response =requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
   
- streamlit.header("View Our Fruit List - Add Your Favorites!")
+streamlit.header('Fruitywise Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+  else:
+       back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
+        
+except URLError as e:
+    streamlit.error()
+    
+
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
            my_cur.execute("select * from fruit_load_list")
